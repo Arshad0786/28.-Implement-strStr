5d410645b6e84cc8e81f83651a43b7b264c53767
Arshad0786 <js07150715@gmail.com>
2020-05-04 11:58:45 +0800

    Adding finished main file that is accepted by leetcode and updated unit test file

--- ImplementstrStr.py
+++ ImplementstrStr.py
@@ -1,3 +1,18 @@
 class Solution:
     def strStr(self, haystack, needle):
-        pass
+        if not needle:
+            return 0
+        if(len(needle) > len(haystack)):
+            return -1
+        SameFlag = False
+        for haystackIndex in range(len(haystack)):
+            if(haystack[haystackIndex] == needle[0]):
+                SameFlag = True
+                for needleIndex in range(len(needle)):
+                    if(haystackIndex + needleIndex == len(haystack)):
+                        return -1
+                    if(haystack[haystackIndex + needleIndex] != needle[needleIndex]):
+                        SameFlag = False
+                if(SameFlag):
+                    return haystackIndex
+        return -1
--- unit_test.py
+++ unit_test.py
@@ -9,6 +9,54 @@ class RemoveDuplicatesfromSortedArrayTest(unittest.TestCase):
         self.needle = "ll"
         self.assertEqual(temp.strStr(self.heystack,self.needle), 2)
 
+    def test_no_needle_found(self):
+        temp = Solution()
+        self.heystack = "HelloWorld"
+        self.needle = "aa"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), -1)
+
+    def test_haystack_is_needle(self):
+        temp = Solution()
+        self.heystack = "needle"
+        self.needle = "needle"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), 0)
+
+    def test_empty_needle(self):
+        temp = Solution()
+        self.heystack = "aword"
+        self.needle = ""
+        self.assertEqual(temp.strStr(self.heystack,self.needle), 0)
+
+    def test_needle_in_the_front(self):
+        temp = Solution()
+        self.heystack = "aabbccdd"
+        self.needle = "aa"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), 0)
+
+    def test_needle_in_the_end(self):
+        temp = Solution()
+        self.heystack = "aabbccdd"
+        self.needle = "dd"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), 6)
+
+    def test_needle_in_the_middle(self):
+        temp = Solution()
+        self.heystack = "aabbccddee"
+        self.needle = "cc"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), 4)
+
+    def test_needle_larger_than_haystack(self):
+        temp = Solution()
+        self.heystack = "aaa"
+        self.needle = "aaaa"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), -1)
+
+    def test_needle_euqal_last_letter(self):
+        temp = Solution()
+        self.heystack = "abcdefg"
+        self.needle = "gigeo"
+        self.assertEqual(temp.strStr(self.heystack,self.needle), -1)
+
 
 if __name__ == "__main__":
     unittest.main()
